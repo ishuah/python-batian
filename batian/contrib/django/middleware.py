32a1a8b0b05d25fe610a7168cbd34ac76ffff797
ishuah91 <ishuah91@gmail.com>
2015-09-04 12:49:01 +0300

    some clean up

--- batian/contrib/django/middleware.py
+++ batian/contrib/django/middleware.py
@@ -11,16 +11,19 @@ class CatchMiddleware(object):
 	def process_request(self, request):
 		request.start_time = time.time()
 
+	def process_view(self, request, view_func, view_args, view_kwargs):
+		request._batian_view_func = view_func
+
 	def process_response(self, request, response):
-		self.log_event(request, response, connection.queries)
+		self._log_event(request, response, connection.queries)
 		return response
 
-	def process_exception(request, exception):
-		self.log_exception(request, exception)
+	def process_exception(self, request, exception):
+		self._log_exception(request, exception)
 
 	@threads(3)
-	def log_event(self, request, response, queries):
-		
+	def _log_event(self, request, response, queries):
+		view_name = self._extract_view_name(request._batian_view_func)
 		data = [{
 				"measurement": "requests",
 				"data": {
@@ -28,21 +31,21 @@ class CatchMiddleware(object):
 					"host": request.get_host(),
 					"path": request.get_full_path(),
 					"method": request.method,
+					"view": view_name,
 					"status_code": response.status_code,
 					"response_time": time.time() - request.start_time
 				},
 				"timestamp": int(round(time.time()*1000))
 			}]
 		for query in queries:
-			extract = re.findall('"([^"]*)"', query['sql'])
-			table = extract[0] + "." + extract[1]
 			qdata = {
 				"measurement": "database_queries",
 				"data": {
 					"app": settings.BATIAN_APP_NAME,
 					"host": request.get_host(),
 					"path": request.get_full_path(),
-					"table": table,
+					"sql": query['sql'].split('WHERE')[0],
+					"view": view_name,
 					"response_time": float(query['time'])
 				},
 				"timestamp": int(round(time.time()*1000))
@@ -53,20 +56,27 @@ class CatchMiddleware(object):
 		requests.post(self.SERVER_URL, data=json.dumps(data), headers={'content-type': 'application/json'})
 
 
-
 	@threads(3)
-	def log_exception(self, request, exception):
+	def _log_exception(self, request, exception):
 		data = [{
 				"measurement": "exceptions",
-				"tags": {
+				"data": {
 					"app": settings.BATIAN_APP_NAME,
 					"host": request.get_host(),
 					"path": request.get_full_path(),
 					"method": request.method,
 					"message": exception.message
 				},
-				"fields": {
-					"fatal": 1
-				}
+				"timestamp": int(round(time.time()*1000))
 			}]
 		requests.post(self.SERVER_URL, data=json.dumps(data), headers={'content-type': 'application/json'})
+
+	def _extract_view_name(self, view_func):
+		module = view_func.__module__
+
+		if hasattr(view_func, '__name__'):
+			view_name = view_func.__name__
+		else:  
+			view_name = view_func.__class__.__name__
+
+		return '{0}.{1}'.format(module, view_name)
